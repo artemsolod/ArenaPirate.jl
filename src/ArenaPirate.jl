45989c37f6ae7bc97a966eee8846ae451e146021
Artem Solod <no@email.no>
2025-09-28 11:44:34 +0000

    refactor

--- src/ArenaPirate.jl
+++ src/ArenaPirate.jl
@@ -1,48 +1,69 @@
 module ArenaPirate
 import Mmap: MADV_HUGEPAGE
+using Base.Threads
 
 export activate_arena!, @arena, @noarena, copy_from_arena, mtmap_arena, map_arena
 
-# can we assert arena is free after the end of use? this happens automatically, doesn't it? so what can be asserted?
-# have immutable arena to save on indirection?
+# allocation of (n, m) shapes?
+# strict arena option (without fallbacks)?
+# lifetimes?
+# activate on import default
+# allow resizing, ENV variable for first init?
+
+# how to ensure `copy_from_arena` before arena is gone?
+# should @noarena within @arena be ignored? can we have @banarena instead? 
+#    if @noarena is ignored within @areana - what is it's purpose then?
+#    imagine all is good except in one place you need to push!. wouldn't you want @noarena
+#    to help?
 
 # Strict arena - no nesting allowed?
 # ! to make sure we can't introduce a new arena inside @noarena block, we may use 3 states for it: Nothing | Arena | Noarena
 # what semantics do we want from `@arena(@noarena(@arena(f(x))))`?
 # what if `arena[]` | `arena_pool` manipulations occur within an arena? We definitely don't want that
 
+# ? can `arena` become a function, not macro?
+
+# ! NumaAllocation of memory per thread?
+# can we assert arena is free after the end of use? this happens automatically, doesn't it? so what can be asserted?
+
+# have immutable arena to save on indirectiona ?
+
+
+# problematic like this? locking arena_pool on each iteration?
+# @arena for x in xs
+#     ...
+# end
+
 mutable struct Arena
     const ptr :: Ptr{Nothing}
     const capacity :: Int
     active :: Bool
-    # call_depth :: Int
-    # @atomic offset :: Int
-    offset :: Int
-    task :: Task
-    debug :: Bool
+    offset :: Int # @atomic ?
+    task :: Task # assigned only when `offset == 0`
 end
 
 const arena = Base.ScopedValues.ScopedValue{Union{Nothing, Arena}}(nothing)
-const arena_pool  = (; lock=Threads.SpinLock(), pool=Vector{ArenaPirate.Arena}[]) # pool should have try to avoid moving across cores / threads / tasks
+const arena_pool  = (;
+    lock=ReentrantLock(), 
+    pool=[Arena[] for tid in 1:(nthreads(:interactive) + nthreads(:default))]
+) # pool should try to avoid moving across cores / threads / tasks
 
 function Arena(capacity=2^27)
     if Sys.islinux()
         ptr = @ccall pvalloc(capacity::Csize_t)::Ptr{Cvoid} # depreceated pvalloc
         retcode = @ccall madvise(ptr::Ptr{Cvoid}, capacity::Csize_t, MADV_HUGEPAGE::Cint) :: Cint
-        @assert iszero(retcode)           
+        iszero(retcode) || @warn "Madvise HUGEPAGE for arena memory failed"
     else
         ptr = Libc.malloc(capacity)
     end
-    finalizer(Arena(ptr, capacity, true, 0, current_task(), false)) do a
+    finalizer(Arena(ptr, capacity, true, 0, current_task())) do a
         Libc.free(a.ptr)
     end
 end
 
 function reset_arena!(arena, tgt_offset=0)
     arena.active = true
-    # arena.call_depth = 0
     arena.offset = tgt_offset
-    # @atomic arena.offset = tgt_offset
     return arena
 end
 
@@ -50,7 +71,7 @@ end
 # have a dict of currently used arenas and pick it up again to allow for @arena -> @noarena -> @arena pattern
 macro arena(call)
     quote
-        tid = Threads.threadid()
+        tid = threadid()
         if isnothing(arena[])
             arena2use = lock(arena_pool.lock) do
                 if isempty(arena_pool.pool[tid])
@@ -67,37 +88,33 @@ macro arena(call)
         offset_orig = arena2use.offset
         active_orig = arena2use.active
         try
-            # arena2use.call_depth += 1
             arena2use.active = true
-            Base.ScopedValues.with(arena => arena2use) do # is deep nesting a problem? hopefully not
+            Base.ScopedValues.with(arena => arena2use) do
                 $(esc(call))
             end
         finally
-            # @atomic arena2use.offset = offset_orig
             arena2use.offset = offset_orig
             arena2use.active = active_orig 
-            # arena2use.call_depth -= 1
-            # if iszero(arena2use.call_depth)
-            if isnothing(arena[])
-                @assert iszero(arena2use.offset)
+            if isnothing(arena[]) # outermost use done, arena can be returned to pool
+                iszero(arena2use.offset) || error("Arena's cursor should have returned to zero. Expect bugs.")
                 reset_arena!(arena2use)
                 # can this get arena allocated?! if we were in a nested call...
-                Base.ScopedValues.with(arena => nothing) do
-                    lock(arena_pool.lock) do  # what if we did @noarena here?
-                        push!(arena_pool.pool[tid], arena2use)
-                    end
+                # Base.ScopedValues.with(arena => nothing) do
+                lock(arena_pool.lock) do  # what if we did @noarena here?
+                    push!(arena_pool.pool[tid], arena2use)
                 end
+                # end
             end
         end
     end
 end
 
 macro noarena(call)
+    # if we have arena, switch status to inactive
     quote
         if isnothing(arena[])
-            Base.ScopedValues.with(arena => nothing) do
-                $(esc(call))
-            end
+            $(esc(call))
+        # elseif arena[].task !== current_task()
         else
             arena2use = arena[]
             active_orig = arena2use.active
@@ -112,10 +129,13 @@ macro noarena(call)
 end
 
 function copy_from_arena(x)
-    @noarena(copy(x))
+    # ugly but ensures we don't return arena to pool prior to copying
+    # simply `with(arena => nothing)` will interfere with nested `@arena` uses
+    @arena(@noarena(copy(x))) 
 end
 
 @inline function alloc_arena!(arena :: Arena, ::Type{T}, nels) where T # fails without Arena typed explicitly
+    # @show T, nels
     if (current_task() === arena.task) && arena.active
         # since no other task uses this arena we don't worry about concurrency bugs
         elsz = Base.aligned_sizeof(T)
@@ -124,10 +144,10 @@ end
         offset_cand = (offset_cand + (elsz - 1)) & ~(elsz - 1) # enforce alignment
         mem_avail = arena.capacity - offset_cand
         if mem_avail >= nbytes
-            # offset_new = @atomic arena.offset += nbytes
             arena.offset = offset_cand
-            ptr_cur = arena.ptr + arena.offset - nbytes
-            mem = @ccall jl_ptr_to_genericmemory(Memory{T}::Any, ptr_cur::Ptr{Cvoid}, nbytes::Csize_t, 0::Cint) :: Memory{T}
+            ptr_cur = convert(Ptr{T}, arena.ptr + arena.offset - nbytes)
+            # mem = @ccall jl_ptr_to_genericmemory(Memory{T}::Any, ptr_cur::Ptr{Cvoid}, nels::Csize_t, 0::Cint) :: Memory{T}
+            mem = unsafe_wrap(Memory{T}, ptr_cur, nels; own=false) :: Memory{T}
             return mem
         end
     end
@@ -136,62 +156,49 @@ end
     @ccall jl_alloc_genericmemory(Memory{T}::Any, nels::Csize_t)::Memory{T}
 end
 
-function mtmap_arena(f, els)
-    fetch.([Threads.@spawn (@arena f(el)) for el in els])
+@inline function Memory{T}(::UndefInitializer, m::Int64) where T <: Any
+    if isbitstype(T) && !isnothing(arena[]) && (m * sizeof(T) > 2000) #(m >= (2^12 ÷ sizeof($T))) # GC_PERM_POOL_LIMIT, GC_MAX_SZCLASS
+        alloc_arena!(arena[], T, m)
+    else
+        @ccall jl_alloc_genericmemory(Memory{T}::Any, m::Csize_t)::Memory{T}
+    end
 end
 
-function map_arena(f, els)
-    map(x -> @arena(f(x)), els)
-end
+
+map_arena(f, els) = map(x -> @arena(f(x)), els)
+mtmap_arena(f, els) = fetch.([@spawn (@arena f(el)) for el in els])
+
+
+
 
 # function clear_arena_pool!(pool_sz=0)
 #     resize!(arena_pool.pool, pool_sz)
 # end
 
-function activate_arena!(types=(Float64, Int64, UInt8))
-    for tid in 1:(Threads.nthreads(:interactive) + Threads.nthreads(:default) - length(arena_pool.pool))
-        push!(arena_pool.pool, Arena[])
-    end
-    @eval begin
-        @inline function Memory{T}(::UndefInitializer, m::Int64) where T <: Any
-            if isbitstype(T) && !isnothing(arena[]) && (m * sizeof(T) > 2000) #(m >= (2^12 ÷ sizeof($T))) # GC_PERM_POOL_LIMIT, GC_MAX_SZCLASS
-                if arena[].debug
-                    @show T, m
-                end
-                alloc_arena!(arena[], T, m)
-            else
-                @ccall jl_alloc_genericmemory(Memory{T}::Any, m::Csize_t)::Memory{T}
-            end
-        end
-    end
-end
-
-
-# function activate_arena!(types=(Float64, Int64, UInt8))
-#     for tid in 1:Threads.nthreads()
+# function activate_arena!() #types=(Float64, Int64, UInt8))
+#     for tid in 1:(nthreads(:interactive) + nthreads(:default) - length(arena_pool.pool))
 #         push!(arena_pool.pool, Arena[])
 #     end
-#     for T in types
-#         @eval begin
-#             @inline function Memory{$T}(::UndefInitializer, m::Int64)
-#                 if !isnothing(arena[]) && (m * sizeof($T) > 2000) #(m >= (2^12 ÷ sizeof($T))) # GC_PERM_POOL_LIMIT, GC_MAX_SZCLASS
-#                     alloc_arena!(arena[], $T, m)
-#                 else
-#                     @ccall jl_alloc_genericmemory(Memory{$T}::Any, m::Csize_t)::Memory{$T}
-#                 end
-#             end
-#         end
+#     @eval begin
 #     end
 # end
-
-# @inline function Memory{T}(::UndefInitializer, m::Int64) where T
-#     if !isnothing(arena[]) && (m * sizeof($T) > 2000) #(m >= (2^12 ÷ sizeof($T))) # GC_PERM_POOL_LIMIT, GC_MAX_SZCLASS
-#         # @show "hi"
-#         alloc_arena!(arena[], T, m)
-#     else
-#         @ccall jl_alloc_genericmemory(Memory{T}::Any, m::Csize_t)::Memory{T}
-#     end
-# end
-
 
 end # module ArenaPirate
+
+
+
+
+
+# can "remove" `@noarena` in this pattern? no, inner arena gets reset once out of scope
+# @arena begin
+#     res = []
+#     @arena for x in xs
+#         y = f(x)
+#         @noarena push!(res, y)
+#     end
+#     sum(res)
+# end
+# Would probably be good to use memory "device" Arena, not cpu.
+# ! what we want is to ask `is_arena` on data. the problem here is memory fragmentation though.
+#   ! stil breaks arena logic of reverting back to outer scope context
+# Maybe needs having memory "age"
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
-using ArenaPirate
 using Test
 
-@testset "ArenaPirate.jl" begin
-    # Write your tests here.
+function f(N)
+    sum(collect(1:N) + collect(1:N))
 end
+
+@testset "ArenaPirate.jl" begin
+    x_noarena = f(2000)
+
+    using ArenaPirate
+    x_arena = f(2000)
+
+    @test x_noarena == x_arena
+end
+
+
+# edge case: `@arena @spawn @arena f(x)`
+# to copy f(x) we need to put copy inside `@spawn` (or better even inside @arena)
+
+
+
