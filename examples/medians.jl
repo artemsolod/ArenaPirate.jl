de7707f79387733b7215cddb547e2af2f93a1d00
Artem Solod <no@email.no>
2025-09-29 16:32:35 +0000

    example

--- examples/medians.jl
+++ examples/medians.jl
@@ -22,10 +22,10 @@ nruns = 32
 f(data)
 res_f = @time f(data)
 
-repeated_f(data)
+repeated_f(data, 1)
 res_rep_f = @time repeated_f(data, nruns)
 
-repeated_f_mt(data)
+repeated_f_mt(data, 1)
 res_rep_f_mt = @time repeated_f_mt(data, nruns)
 
 using ArenaPirate
@@ -50,10 +50,10 @@ println("____ Arena benchmarks:")
 f_arena(data)
 res_f_arena = @time f_arena(data)
 
-repeated_f_arena(data)
+repeated_f_arena(data, 1)
 res_rep_f_arena = @time repeated_f_arena(data, nruns)
 
-repeated_f_mt_arena(data)
+repeated_f_mt_arena(data, 1)
 res_rep_f_mt_arena = @time repeated_f_mt_arena(data, nruns)
 
 @assert res_f == res_f_arena
